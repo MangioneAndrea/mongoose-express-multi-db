f1d1d22ea671ba3a6cd8c98cedf2fb9c7737392d
Andrea Mangione <35958935+MangioneAndrea@users.noreply.github.com>
2022-07-29 20:09:48 +0200

    add docs

--- src/index.ts
+++ src/index.ts
@@ -3,31 +3,56 @@ import type {Express, NextFunction, Request, Response} from "express"
 
 import {fileHasMongooseDeclaration, walk} from "./util";
 
-
+/**
+ * Optional configuration for the package
+ */
 type OptionalConfig = {
     getDBName?: (req: Request) => string,
     connectOptions?: mongoose.ConnectOptions
 }
-
+/**
+ * Required configuration for the package
+ */
 type RequiredConfig = {
+    /**
+     * uri for the mongoDb. This shouldn't contain the db at the end
+     */
     mongoUri: string,
+    /**
+     * Path where to search for the models. The narrower, the faster. Not all files will be imported. Only those containing models
+     */
     modelsPaths: string | Array<string>
 }
 
 type Config = OptionalConfig & RequiredConfig
 
+/**
+ * Configurations used as callback for the optional config
+ */
 const defaultConfig: Required<OptionalConfig> = {
+    /**
+     * The database is equal to the origin or localhost
+     */
     getDBName(req: Request): string {
         return req?.headers?.origin ?? "localhost";
     },
+    /**
+     * No connectOptions
+     */
     connectOptions: {}
 }
 
-
+/**
+ * This is how the models definition looks like.
+ * Both models and types are accepted
+ */
 interface KnownModels {
     [key: string]: Model<any> | any
 }
 
+/**
+ * Object containing each db info
+ */
 export class Tenant<T extends KnownModels = {}> {
 
     /** @internal */
@@ -39,23 +64,37 @@ export class Tenant<T extends KnownModels = {}> {
     /** @internal */
     readonly #name: string
 
+    /**
+     * Name of the database
+     */
     get name(): string {
         return this.#name;
     }
 
+    /**
+     * Map of all found models
+     */
     get models(): Map<string, Model<any>> {
         return this.#models;
     }
 
+    /**
+     * Get reference to model. The string defined in the models will provide typings
+     * @see KnownModels
+     */
     getModel<K extends keyof T, C extends T[K]>(modelName: K): Model<C extends Model<infer N> ? N : T[K]>;
     getModel(modelName: string): Model<any> | undefined {
         return this.models.get(modelName)
     }
 
+    /**
+     * Native mongoose connection
+     */
     get connection(): mongoose.Connection {
         return this.#connection;
     }
 
+    /** @internal  */
     constructor(name: string, connection: mongoose.Connection, schemas: Map<string, Schema>) {
         this.#connection = connection
         this.#name = name;
@@ -67,16 +106,26 @@ export class Tenant<T extends KnownModels = {}> {
     }
 }
 
-
+/**
+ * Object containing all references to the databases. The pool gets bigger when more people from different tenants
+ * try to connect. The references are used to reuse connections but also to allow to cleanup mongoose
+ * @internal
+ */
 class Pool<T extends KnownModels> {
+    /** @internal  */
     readonly #mongoURI: string
+    /** @internal  */
     #schemas: Map<string, Schema> = new Map()
+    /** @internal  */
     #setReady: undefined | { resolve: (() => boolean), reject: ((e: Error) => Error) };
+    /** @internal  */
     readonly isReady = new Promise((resolve: () => boolean, reject: (e: Error) => Error) => {
         this.#setReady = {resolve, reject};
     });
+    /** @internal  */
     #tenants: Map<string, Tenant<T>> = new Map();
 
+    /** @internal  */
     constructor(configs: RequiredConfig) {
         this.#mongoURI = configs.mongoUri.replace(/\/$/, "")
         this.#initModels(configs.modelsPaths).then(() => {
@@ -86,6 +135,7 @@ class Pool<T extends KnownModels> {
         })
     }
 
+    /** @internal  */
     async #initModels(paths: Array<string> | string) {
         const files = walk(paths)
 
@@ -97,20 +147,24 @@ class Pool<T extends KnownModels> {
         }
     }
 
+    /** @internal  */
     #has(origin: string): boolean {
         return this.#tenants.has(origin)
     }
 
+    /** @internal  */
     async clear() {
         const res = await Promise.all(Array.from(this.#tenants.values()).map(tenant => tenant.connection.close(true)))
         this.#tenants.clear()
     }
 
+    /** @internal  */
     async #add(origin: string, options: ConnectOptions) {
         const connection = await mongoose.createConnection(`${this.#mongoURI}/${origin}`, {}).asPromise()
         this.#tenants.set(origin, new Tenant(origin, connection, this.#schemas));
     }
 
+    /** @internal  */
     async connect(origin: string, options: ConnectOptions): Promise<Tenant<T>> {
         if (!this.#has(origin)) {
             await this.#add(origin, options);
@@ -119,12 +173,18 @@ class Pool<T extends KnownModels> {
     }
 }
 
-
+/**
+ * Middleware to use direct under express as `app.use(middleware(props))`
+ * @export
+ */
 const middleware = <T extends KnownModels>(configs: Config) => {
     const finalConfigs: Required<Config> = {...defaultConfig, ...configs}
     const pool = new Pool(configs)
 
 
+    /**
+     * This is the middleware itself, wrapped in a function to provide setting directly as oneliner
+     */
     async function MongooseMultiDBMiddleware(req: Request, res: Response, next: NextFunction) {
         try {
             await pool.isReady
@@ -137,13 +197,19 @@ const middleware = <T extends KnownModels>(configs: Config) => {
         }
     }
 
+    // This reference is used to cleanup
     MongooseMultiDBMiddleware.pool = pool;
 
     return MongooseMultiDBMiddleware
 }
-
+/**
+ * Cleanup function to ensure mongoose disconnects from the server
+ * @param app express app where to close the connections from
+ */
 export const killMiddlewareConnections = async (app: Express) => {
+    // Search for the mongoose middleware
     const middleware = (app.stack || app._router.stack).find((el) => el.name === "MongooseMultiDBMiddleware");
+    // Do not throw errors if the middleware was not (yet) applied
     if (!middleware) return;
     await middleware.handle.pool.clear()
 }
